9350570c3f97357873387af64a843b85db04aa5b
yuki3738 <y.minamiya@gmail.com>
2015-04-29 18:09:00 +0900

    下準備した

--- config/application.rb
+++ config/application.rb
@@ -30,5 +30,11 @@ module ReactSample
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.generators do |g|
+      g.template_engine :slim # or :haml / :erb
+      g.assets false
+      g.helpers false
+    end
   end
 end
